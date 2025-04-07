f5452f9ecf8e63b861f264d799bf9eeb2b5f2045
fikas02 <121714463+fikas02@users.noreply.github.com>
2025-04-07 23:37:34 +0700

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -12,11 +12,12 @@ ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
 auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-# Daftar konten tweet
+# Daftar konten tweet (urut sesuai jadwal)
 tweet_contents = [
     "1. OPEN RESELLER! 🌟\nFH buka 07.00 - 03.00, 3 admin, aplikasi 70+, garansi mostly 0-1d! No fee, no target! Cek PL dulu! 💬 Tanyakan langsung ke @xiaojdun di Twitter atau WA di bio! #OpenReseller #BisnisOnline #Aplikasi",
     "2. OPEN RESELLER! Halo, kak! FH saya open dari 07.00 - 03.00 subuh, ada 3 admin fsr, aplikasi 70+ dan garansi mostly 0-1d! bisa kepoin pl nya dulu🤍feel free to ask buat ress baru! last, no fee no target! bisa tanya\" ke twt @xiaojdun atau untuk fsr ke WA di bio @xiaojdun ya",
-    "3. akuu open ress loh kakk"
+    "3. akuu open ress loh kakk",
+    "4. aku onn"  # Tambahan baru untuk 23:30 WIB
 ]
 
 # Fungsi posting tweet
@@ -30,12 +31,14 @@ def post_tweet(message):
 # Waktu UTC sekarang
 current_time = datetime.datetime.now().strftime("%H:%M")
 
-# Jadwal posting
+# Jadwal posting (UTC)
 if current_time == "17:00":  # 00:00 WIB
     post_tweet(tweet_contents[0])
 elif current_time == "18:00":  # 01:00 WIB
     post_tweet(tweet_contents[1])
-elif current_time == "16:30":  # 23:30 WIB
+elif current_time == "19:30":  # 02:30 WIB
     post_tweet(tweet_contents[2])
+elif current_time == "16:30":  # 23:30 WIB (BARU)
+    post_tweet(tweet_contents[3])
 else:
     print(f"🕒 Sekarang jam {current_time} UTC, belum waktunya posting.")
